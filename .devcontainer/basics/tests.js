ec317da47ae85b552ff36d7a1dd8fe0c66b189e4
Komal <156698112+KomalDhiman7@users.noreply.github.com>
2024-06-19 08:09:14 +0000

    conversion of data types

--- .devcontainer/basics/tests.js
+++ .devcontainer/basics/tests.js
@@ -1,4 +1,3 @@
-console.log("komal")
 const accountId = 14453
 let accountEmail ="komaldhiman773@gmail.com"
 var  accountPassword = "12345"
@@ -7,3 +6,7 @@ accountEmail="kd2144532gmail.com"
 accountPassword="122121212"
 accountCity="Mohali"
 console.table([accountId, accountEmail, accountPassword, accountCity])
+/*
+prefer not to use var
+because of issue in block and functional scope
+*/
